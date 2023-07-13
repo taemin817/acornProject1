80f6a716f0453983778e006148ce569d7af8ab47
taemin817 <tmpark11@naver.com>
2023-07-13 12:16:39 +0900

    조별 프로젝트

---
+++ taemin/project-main/wavve/0.mix/js/ek_utilize_wavve.js
@@ -0,0 +1,14 @@
+let flag = true;
+        function ek_btn_click(){
+            let a = document.getElementById("ek_page_voucher_banner");
+            let b = document.getElementById("ek_page_voucher_btn");
+            if(flag==true){
+                a.style.height = "auto";
+                b.style.transform = "rotate(180deg)";
+                flag = false;
+            }else{
+                a.style.height = "110px";
+                b.style.transform = "rotate(0deg)";
+                flag = true;
+            }
+        }
